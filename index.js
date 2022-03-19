76639f56fd8ad0a6bcaf5ded288b24b0a490972f
rouzbehmozafari <rouzbeh.mozafari@yahoo.com>
2022-03-19 17:57:22 +0100

    cors

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(express.json())
 app.use(cookieParser())
 
